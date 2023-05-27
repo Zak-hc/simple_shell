b8f5e7aa6da50c86674ca561823302fae0e0909f
Zack-hc <zhaci001@gmail.com>
2023-05-26 17:52:18 -0700

    tm

--- strtork.c
+++ strtork.c
@@ -21,7 +21,7 @@ buffer = str;
 
 if (buffer == NULL)
 {
-return NULL;
+return (NULL);
 }
 
 token_start = buffer;
@@ -32,6 +32,6 @@ if (buffer != NULL)
 *buffer++ = '\0';
 }
 
-return token_start;
+return (token_start);
 }
 
